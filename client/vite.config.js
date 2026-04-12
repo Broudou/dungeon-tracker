730dd6b450c3bf24738473bac9eac73720594ac5
Broudou <Broudou@users.noreply.github.com>
2026-04-13 00:57:58 +0200

    rolling back my mistake on vite.config.js and adding host option

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,10 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.API_TARGET || 'http://54.37.230.173:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
+    host: true,
   },
 });
